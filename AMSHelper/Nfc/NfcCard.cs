7c2cf1687a323ad3bbfbe420ce559a188825466c
m0quai <michael.kuhlen@lr-systems.com>
2026-08-21 15:12:19 +0200

    Nfc

---
+++ AMSHelper/Nfc/NfcCard.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Diagnostics;
+using Iot.Device.Card;
+using Iot.Device.Pn532;
+using Iot.Device.Rfid;
+
+namespace AMSHelper.Nfc
+{
+   public enum NfcCardType
+   {
+      Unknown = 0,
+
+      // MIFARE Classic
+      MifareClassicMini,
+      MifareClassic1K,
+      MifareClassic4K,
+
+      // MIFARE Ultralight / NTAG-Familie
+      MifareUltralightOrNtag,
+
+      // NXP NTAG21x
+      Ntag213,
+      Ntag215,
+      Ntag216,
+      MifareUltralightOrUnknownType2
+   }
+   public static class NfcCard
+   {
+      public static NfcCardType DetectTagType(Pn532 pn532 ,Data106kbpsTypeA tag)
+      {
+         ushort atqa = tag.Atqa;
+         byte sak = tag.Sak;
+
+         // ---------------------------------------------------------
+         // MIFARE Classic
+         // ---------------------------------------------------------
+
+         // MIFARE Classic 1K
+         if (sak == 0x08)
+         {
+            return NfcCardType.MifareClassic1K;
+         }
+
+         // MIFARE Classic 4K
+         if (sak == 0x18)
+         {
+            return NfcCardType.MifareClassic4K;
+         }
+
+         // MIFARE Mini
+         if (sak == 0x09)
+         {
+            return NfcCardType.MifareClassicMini;
+         }
+
+         // ---------------------------------------------------------
+         // MIFARE Ultralight / NTAG21x
+         //
+         // Diese Familie meldet typischerweise SAK = 0x00.
+         // ATQA/SAK reichen NICHT, um 213/215/216 zu unterscheiden.
+         // ---------------------------------------------------------
+
+         if (sak == 0x00)
+         {
+            NfcCardType ntag =
+                DetectNtag21x(pn532 ,tag.TargetNumber);
+
+            if (ntag != NfcCardType.Unknown)
+            {
+               return ntag;
+            }
+
+            return NfcCardType.MifareUltralightOrNtag;
+         }
+
+         return NfcCardType.Unknown;
+      }
+      public static string GetTagTypeName(NfcCardType tagType)
+      {
+         switch (tagType)
+         {
+            case NfcCardType.MifareClassicMini:
+               return "MifareClassicMini";
+
+            case NfcCardType.MifareClassic1K:
+               return "MifareClassic1K";
+
+            case NfcCardType.MifareClassic4K:
+               return "MifareClassic4K";
+
+            case NfcCardType.MifareUltralightOrUnknownType2:
+               return "MifareUltralightOrUnknownType2";
+
+            case NfcCardType.Ntag213:
+               return "Ntag213";
+
+            case NfcCardType.Ntag215:
+               return "Ntag215";
+
+            case NfcCardType.Ntag216:
+               return "Ntag216";
+
+            default:
+               return "Unknown";
+         }
+      }
+      public static byte[] ReadNtagPages(Pn532 pn532 ,byte targetNumber ,byte startPage)
+      {
+         // NTAG21x READ command:
+         // 0x30 + Startseite
+         // Antwort: 4 Seiten = 16 Byte
+         byte[] command =
+         {
+            0x30,
+            startPage
+         };
+
+         byte[] response = new byte[16];
+
+         var send = new SpanByte(
+             command ,
+             0 ,
+             command.Length);
+
+         var receive = new SpanByte(
+             response ,
+             0 ,
+             response.Length);
+
+         try
+         {
+            int bytesRead = pn532.Transceive(
+                targetNumber ,
+                send ,
+                receive ,
+                NfcProtocol.Mifare);
+
+            if (bytesRead < 16)
+            {
+               Debug.WriteLine("READ fehlgeschlagen. Bytes: " + bytesRead);
+               return null;
+            }
+            return response;
+         }
+         catch (Exception ex)
+         {
+            Debug.WriteLine("NTAG READ Fehler ab Seite " + startPage + ": " + ex.Message);
+            return null;
+         }
+      }
+      public static void DumpNtagPages(byte startPage ,byte[] data)
+      {
+         if (data == null)
+         {
+            return;
+         }
+
+         for (int i = 0; i < 4; i++)
+         {
+            int offset = i * 4;
+
+            Debug.WriteLine("Page " +
+                (startPage + i) +
+                ": " +
+                data[offset + 0].ToString("X2") + "-" +
+                data[offset + 1].ToString("X2") + "-" +
+                data[offset + 2].ToString("X2") + "-" +
+                data[offset + 3].ToString("X2"));
+         }
+      }
+      private static NfcCardType DetectNtag21x(Pn532 pn532 ,byte targetNumber)
+      {
+         // NXP NTAG21x GET_VERSION
+         byte[] command = new byte[]
+         {
+            0x60
+         };
+
+         // GET_VERSION liefert 8 Bytes.
+         byte[] response = new byte[8];
+
+         var commandSpan = new SpanByte(
+             command ,
+             0 ,
+             command.Length);
+
+         var responseSpan = new SpanByte(
+             response ,
+             0 ,
+             response.Length);
+
+         try
+         {
+            int bytesRead = pn532.Transceive(
+                targetNumber ,
+                commandSpan ,
+                responseSpan ,
+                NfcProtocol.Mifare);
+
+            if (bytesRead < 8)
+            {
+               return NfcCardType.Unknown;
+            }
+
+            // -----------------------------------------------------
+            // NTAG21x GET_VERSION Format:
+            //
+            // [0] Header
+            // [1] Vendor ID
+            // [2] Product Type
+            // [3] Product Subtype
+            // [4] Major Version
+            // [5] Minor Version
+            // [6] Storage Size
+            // [7] Protocol Type
+            //
+            // NXP Vendor ID = 0x04
+            // -----------------------------------------------------
+
+            if (response[1] != 0x04)
+            {
+               return NfcCardType.Unknown;
+            }
+
+            // NXP NTAG Product Type
+            if (response[2] != 0x04)
+            {
+               return NfcCardType.Unknown;
+            }
+
+            // NTAG21x Storage Size
+            switch (response[6])
+            {
+               case 0x0F:
+                  return NfcCardType.Ntag213;
+
+               case 0x11:
+                  return NfcCardType.Ntag215;
+
+               case 0x13:
+                  return NfcCardType.Ntag216;
+
+               default:
+                  return NfcCardType.Unknown;
+            }
+         }
+         catch
+         {
+            // Ein Ultralight-/Type-2-Tag muss GET_VERSION
+            // nicht zwingend unterstützen.
+            return NfcCardType.Unknown;
+         }
+      }
+   }
+}
--- AMSHelper/Program.cs
+++ AMSHelper/Program.cs
@@ -1,214 +1,275 @@
-using System;
 using System.Device.I2c;
-using System.Diagnostics;
-using System.Net.NetworkInformation;
-using System.Threading;
 using Iot.Device.Pn532;
+using Iot.Device.Pn532.ListPassive;
 using nanoFramework.Hardware.Esp32;
+using AMSHelper.Nfc;
 
 namespace AMSHelper
 {
    using System;
    using System.Diagnostics;
-   using System.Net;
    using System.Threading;
-   using nanoFramework.Networking;
+   using Iot.Device.Pn532.RfConfiguration;
 
    namespace AMSHelper
    {
+      //   public class ProgramWLAN
+      //   {
+      //      private const string WifiSsid = "LRWN";
+      //      private const string WifiPassword = "Network@Home4711007";
+
+      //      public static void Mainx()
+      //      {
+      //          Debug.WriteLine("================================");
+      //         Debug.WriteLine("AMSHelper WLAN-Test");
+      //         Debug.WriteLine("================================");
+
+      //         try
+      //         {
+      //            Debug.WriteLine("Verbinde mit WLAN: " + WifiSsid);
+
+      //            bool success = WifiNetworkHelper.ConnectDhcp(
+      //                WifiSsid ,
+      //                WifiPassword ,
+      //                requiresDateTime: false);
+
+      //            if (success)
+      //            {
+      //               Debug.WriteLine("WLAN VERBUNDEN!");
+
+      //               NetworkInterface[] interfaces =
+      //                   NetworkInterface.GetAllNetworkInterfaces();
+
+      //               for (int i = 0; i < interfaces.Length; i++)
+      //               {
+      //                  NetworkInterface ni = interfaces[i];
+
+      //                  if (ni.NetworkInterfaceType ==
+      //                      NetworkInterfaceType.Wireless80211)
+      //                  {
+      //                     Debug.WriteLine("-----------------------------");
+      //                     Debug.WriteLine("WLAN-INTERFACE");
+      //                     Debug.WriteLine("IP:      " + ni.IPv4Address);
+      //                     Debug.WriteLine("Subnetz: " + ni.IPv4SubnetMask);
+      //                     Debug.WriteLine("Gateway: " + ni.IPv4GatewayAddress);
+      //                     Debug.WriteLine("DHCP:    " + ni.IsDhcpEnabled);
+
+      //                     if (ni.IPv4DnsAddresses != null)
+      //                     {
+      //                        for (int d = 0; d < ni.IPv4DnsAddresses.Length; d++)
+      //                        {
+      //                           Debug.WriteLine(
+      //                               "DNS " + (d + 1) + ":   " +
+      //                               ni.IPv4DnsAddresses[d]);
+      //                        }
+      //                     }
+
+      //                     byte[] mac = ni.PhysicalAddress;
+
+      //                     if (mac != null && mac.Length > 0)
+      //                     {
+      //                        string macString = "";
+
+      //                        for (int m = 0; m < mac.Length; m++)
+      //                        {
+      //                           if (m > 0)
+      //                           {
+      //                              macString += ":";
+      //                           }
+
+      //                           macString += mac[m].ToString("X2");
+      //                        }
+
+      //                        Debug.WriteLine("MAC:     " + macString);
+      //                     }
+
+      //                     Debug.WriteLine("-----------------------------");
+      //                     Debug.WriteLine("");
+      //                     Debug.WriteLine("Teste Verbindung zu NBK-01-548...");
+
+      //                     Debug.WriteLine("");
+      //                     Debug.WriteLine("Teste Namensauflösung NBK-01-548...");
+
+      //                     try
+      //                     {
+      //                        IPHostEntry host = Dns.GetHostEntry("NBK-01-548");
+
+      //                        if (host.AddressList != null && host.AddressList.Length > 0)
+      //                        {
+      //                           for (int i1 = 0; i1 < host.AddressList.Length; i1++)
+      //                           {
+      //                              Debug.WriteLine(
+      //                                  "NBK-01-548 -> " +
+      //                                  host.AddressList[i].ToString());
+      //                           }
+      //                        }
+      //                        else
+      //                        {
+      //                           Debug.WriteLine("Keine IP-Adresse gefunden.");
+      //                        }
+      //                     }
+      //                     catch (Exception ex)
+      //                     {
+      //                        Debug.WriteLine("Namensauflösung fehlgeschlagen:");
+      //                        Debug.WriteLine(ex.Message);
+      //                     }
+
+
+      //                  }
+      //               }
+      //            }
+      //            else
+      //            {
+      //               Debug.WriteLine("WLAN-VERBINDUNG FEHLGESCHLAGEN");
+
+      //               Debug.WriteLine(
+      //                   "Status: " +
+      //                   WifiNetworkHelper.Status.ToString());
+
+      //               if (WifiNetworkHelper.HelperException != null)
+      //               {
+      //                  Debug.WriteLine(
+      //                      "Fehler: " +
+      //                      WifiNetworkHelper.HelperException.Message);
+      //               }
+      //            }
+      //         }
+      //         catch (Exception ex)
+      //         {
+      //            Debug.WriteLine("EXCEPTION:");
+      //            Debug.WriteLine(ex.Message);
+      //         }
+
+      //         Debug.WriteLine("Test beendet.");
+
+      //         Thread.Sleep(Timeout.Infinite);
+      //      }
+      //   }
+      //}
       public class Program
       {
-         private const string WifiSsid = "LRWN";
-         private const string WifiPassword = "Network@Home4711007";
+         private const int I2cBus = 1;
+
+         private const int SdaPin = 8;
+         private const int SclPin = 9;
 
          public static void Main()
          {
-             Debug.WriteLine("================================");
-            Debug.WriteLine("AMSHelper WLAN-Test");
-            Debug.WriteLine("================================");
+            Debug.WriteLine("AMSHelper gestartet.");
+            Debug.WriteLine("Initialisiere PN532 über I2C...");
+
+            // ESP32-S3 GPIO-Zuordnung für I2C1
+            Configuration.SetPinFunction(
+                SdaPin ,
+                DeviceFunction.I2C1_DATA);
+
+            Configuration.SetPinFunction(
+                SclPin ,
+                DeviceFunction.I2C1_CLOCK);
+
+            Debug.WriteLine(
+                $"I2C1: SDA=GPIO{SdaPin}, SCL=GPIO{SclPin}");
+
+            var settings = new I2cConnectionSettings(
+                I2cBus ,
+                Pn532.I2cDefaultAddress ,
+                I2cBusSpeed.StandardMode);
 
             try
             {
-               Debug.WriteLine("Verbinde mit WLAN: " + WifiSsid);
-
-               bool success = WifiNetworkHelper.ConnectDhcp(
-                   WifiSsid ,
-                   WifiPassword ,
-                   requiresDateTime: false);
-
-               if (success)
+               using (I2cDevice i2cDevice = I2cDevice.Create(settings))
+               using (Pn532 pn532 = new Pn532(i2cDevice))
                {
-                  Debug.WriteLine("WLAN VERBUNDEN!");
+                  var firmware = pn532.FirmwareVersion;
 
-                  NetworkInterface[] interfaces =
-                      NetworkInterface.GetAllNetworkInterfaces();
-
-                  for (int i = 0; i < interfaces.Length; i++)
-                  {
-                     NetworkInterface ni = interfaces[i];
-
-                     if (ni.NetworkInterfaceType ==
-                         NetworkInterfaceType.Wireless80211)
-                     {
-                        Debug.WriteLine("-----------------------------");
-                        Debug.WriteLine("WLAN-INTERFACE");
-                        Debug.WriteLine("IP:      " + ni.IPv4Address);
-                        Debug.WriteLine("Subnetz: " + ni.IPv4SubnetMask);
-                        Debug.WriteLine("Gateway: " + ni.IPv4GatewayAddress);
-                        Debug.WriteLine("DHCP:    " + ni.IsDhcpEnabled);
-
-                        if (ni.IPv4DnsAddresses != null)
-                        {
-                           for (int d = 0; d < ni.IPv4DnsAddresses.Length; d++)
-                           {
-                              Debug.WriteLine(
-                                  "DNS " + (d + 1) + ":   " +
-                                  ni.IPv4DnsAddresses[d]);
-                           }
-                        }
-
-                        byte[] mac = ni.PhysicalAddress;
-
-                        if (mac != null && mac.Length > 0)
-                        {
-                           string macString = "";
-
-                           for (int m = 0; m < mac.Length; m++)
-                           {
-                              if (m > 0)
-                              {
-                                 macString += ":";
-                              }
-
-                              macString += mac[m].ToString("X2");
-                           }
-
-                           Debug.WriteLine("MAC:     " + macString);
-                        }
-
-                        Debug.WriteLine("-----------------------------");
-                        Debug.WriteLine("");
-                        Debug.WriteLine("Teste Verbindung zu NBK-01-548...");
-
-                        Debug.WriteLine("");
-                        Debug.WriteLine("Teste Namensauflösung NBK-01-548...");
-
-                        try
-                        {
-                           IPHostEntry host = Dns.GetHostEntry("NBK-01-548");
-
-                           if (host.AddressList != null && host.AddressList.Length > 0)
-                           {
-                              for (int i1 = 0; i1 < host.AddressList.Length; i1++)
-                              {
-                                 Debug.WriteLine(
-                                     "NBK-01-548 -> " +
-                                     host.AddressList[i].ToString());
-                              }
-                           }
-                           else
-                           {
-                              Debug.WriteLine("Keine IP-Adresse gefunden.");
-                           }
-                        }
-                        catch (Exception ex)
-                        {
-                           Debug.WriteLine("Namensauflösung fehlgeschlagen:");
-                           Debug.WriteLine(ex.Message);
-                        }
-
-
-                     }
-                  }
-               }
-               else
-               {
-                  Debug.WriteLine("WLAN-VERBINDUNG FEHLGESCHLAGEN");
-
-                  Debug.WriteLine(
-                      "Status: " +
-                      WifiNetworkHelper.Status.ToString());
-
-                  if (WifiNetworkHelper.HelperException != null)
+                  if (firmware == null)
                   {
                      Debug.WriteLine(
-                         "Fehler: " +
-                         WifiNetworkHelper.HelperException.Message);
+                         "FEHLER: PN532 antwortet, aber Firmware-Version konnte nicht gelesen werden.");
                   }
+                  else
+                  {
+                     Debug.WriteLine("PN532 erkannt.");
+                     Debug.WriteLine(
+                         $"Chip-ID: 0x{firmware.IdentificationCode:X2}");
+                     Debug.WriteLine(
+                         $"Firmware: {firmware.Version}");
+                     Debug.WriteLine(
+                         $"Ist PN532: {firmware.IsPn532}");
+                  }
+                  Debug.WriteLine("");
+
+                  var retries = new MaxRetriesMode
+                  {
+                     MaxRetryAnswerToReset = 0x00 ,
+                     MaxRetryPSL = 0x00 ,
+                     MaxRetryPassiveActivation = 0x01
+                  };
+
+                  pn532.SetMaxRetriesInitialization(retries);
+
+                  while (true)
+                  {
+                     try
+                     {
+                        byte[] retData = pn532.ListPassiveTarget(MaxTarget.One ,TargetBaudRate.B106kbpsTypeA);
+
+                        if (retData != null && retData.Length > 1)
+                        {
+                           // Erstes Byte = Anzahl gefundener Tags.
+                           var tagData = new SpanByte(
+                               retData ,
+                               1 ,
+                               retData.Length - 1);
+
+                           var tag = pn532.TryDecode106kbpsTypeA(tagData);
+
+                           if (tag != null)
+                           {
+
+                              NfcCardType tagType = NfcCard.DetectTagType(pn532 ,tag);
+
+                              Debug.WriteLine("--------------------------");
+                              Debug.WriteLine("Target: " + tag.TargetNumber);
+                              Debug.WriteLine("ATQA:   " + tag.Atqa);
+                              Debug.WriteLine("SAK:    " + tag.Sak);
+                              Debug.WriteLine("UID:    " + BitConverter.ToString(tag.NfcId));
+                              Debug.WriteLine("Type:   " + NfcCard.GetTagTypeName(tagType));  
+                              Debug.WriteLine("--------------------------");
+
+                              if( tagType == NfcCardType.Ntag215)
+                              {
+                                 byte[] ntagData = NfcCard.ReadNtagPages(pn532, tag.TargetNumber, 0);
+                                 NfcCard.DumpNtagPages(0, ntagData);
+                              }
+
+                              pn532.ReleaseTarget(tag.TargetNumber);
+                           }
+                        }
+                        else
+                        {
+                           Debug.WriteLine("Keine Tags gefunden.");
+                        }
+                     }
+                     catch (Exception ex)
+                     {
+                        Debug.WriteLine("TAG-LESEFEHLER:");
+                        Debug.WriteLine(ex.Message);
+                     }
+
+//                     Thread.Sleep(200);
+                  }
+
                }
             }
             catch (Exception ex)
             {
-               Debug.WriteLine("EXCEPTION:");
+               Debug.WriteLine("PN532 FEHLER:");
                Debug.WriteLine(ex.Message);
             }
-
-            Debug.WriteLine("Test beendet.");
 
             Thread.Sleep(Timeout.Infinite);
          }
       }
    }
-   //public class ProgramX
-   //{
-   //   private const int I2cBus = 1;
-
-   //   private const int SdaPin = 8;
-   //   private const int SclPin = 9;
-
-   //   public static void MainI2C()
-   //   {
-   //      Debug.WriteLine("AMSHelper gestartet.");
-   //      Debug.WriteLine("Initialisiere PN532 über I2C...");
-
-   //      // ESP32-S3 GPIO-Zuordnung für I2C1
-   //      Configuration.SetPinFunction(
-   //          SdaPin ,
-   //          DeviceFunction.I2C1_DATA);
-
-   //      Configuration.SetPinFunction(
-   //          SclPin ,
-   //          DeviceFunction.I2C1_CLOCK);
-
-   //      Debug.WriteLine(
-   //          $"I2C1: SDA=GPIO{SdaPin}, SCL=GPIO{SclPin}");
-
-   //      var settings = new I2cConnectionSettings(
-   //          I2cBus ,
-   //          Pn532.I2cDefaultAddress ,
-   //          I2cBusSpeed.StandardMode);
-
-   //      try
-   //      {
-   //         using (I2cDevice i2cDevice = I2cDevice.Create(settings))
-   //         using (Pn532 pn532 = new Pn532(i2cDevice))
-   //         {
-   //            var firmware = pn532.FirmwareVersion;
-
-   //            if (firmware == null)
-   //            {
-   //               Debug.WriteLine(
-   //                   "FEHLER: PN532 antwortet, aber Firmware-Version konnte nicht gelesen werden.");
-   //            }
-   //            else
-   //            {
-   //               Debug.WriteLine("PN532 erkannt.");
-   //               Debug.WriteLine(
-   //                   $"Chip-ID: 0x{firmware.IdentificationCode:X2}");
-   //               Debug.WriteLine(
-   //                   $"Firmware: {firmware.Version}");
-   //               Debug.WriteLine(
-   //                   $"Ist PN532: {firmware.IsPn532}");
-   //            }
-   //         }
-   //      }
-   //      catch (Exception ex)
-   //      {
-   //         Debug.WriteLine("PN532 FEHLER:");
-   //         Debug.WriteLine(ex.Message);
-   //      }
-
-   //      Thread.Sleep(Timeout.Infinite);
-   //   }
-   //}
 }
