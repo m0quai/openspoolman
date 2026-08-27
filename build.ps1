9b68d7735f95680debab16e5a4b820aa67bd0f04
m0quai <michael.kuhlen@lr-systems.com>
2026-08-27 13:15:55 +0200

    Always generate fresh build numbers

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ $buildNumber = $now.ToString("yyyyMMdd.HHmmss")
 $env:BUILD_NUMBER = $buildNumber
 Write-Host "Building OpenSpoolMan with BUILD_NUMBER=$buildNumber"
 
-docker compose build --build-arg "BUILD_NUMBER=$buildNumber" openspoolman
+docker compose build --no-cache --build-arg "BUILD_NUMBER=$buildNumber" openspoolman
 if (-not $NoStart) {
     docker compose up -d openspoolman
 }
