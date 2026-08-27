b67aa2c316e589b6e8b034b87cf6379f5cd910e9
m0quai <michael.kuhlen@lr-systems.com>
2026-08-27 12:46:33 +0200

    Add build number to startup version

--- app_custom.py
+++ app_custom.py
@@ -148,12 +148,12 @@ from bambu_auth_routes import bp as bambu_cloud_bp
 from nfc_routes import bp as ams_nfc_bp
 from flask import jsonify, redirect, request, url_for, render_template, send_from_directory
 import mqtt_bambulab
-from __version__ import __version__
+from __version__ import __build_number__, __version__
 import tools_3mf as _tools_3mf
 import filament_usage_tracker as _filament_usage_tracker
 from logger import log as _log
 
-_log(f"OpenSpoolMan version {__version__} starting")
+_log(f"OpenSpoolMan version {__version__} (Build {__build_number__}) starting")
 
 app.register_blueprint(bambu_cloud_bp)
 app.register_blueprint(ams_nfc_bp)
