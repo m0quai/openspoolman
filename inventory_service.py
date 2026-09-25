0ce246f9667b835934347ebd18d43dd9b6fab26d
m0quai <Michael.Kuhlen@lr-systems.com>
2026-09-25 16:43:26 +0200

    fix: align AMS timestamps with local timezone

--- inventory_service.py
+++ inventory_service.py
@@ -218,13 +218,13 @@ def augmentTrayDataWithSpoolMan(spool_list, tray_data, ams_id, tray_id):
         tray_type_clean = (tray_data["tray_type"] or "").strip()
 
       if "last_used" in spool:
-        tray_data["last_used_iso"] = spool.get("last_used")
         try:
             dt = datetime.strptime(spool["last_used"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=ZoneInfo("UTC"))
         except ValueError:
             dt = datetime.strptime(spool["last_used"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=ZoneInfo("UTC"))
 
         local_time = dt.astimezone()
+        tray_data["last_used_iso"] = local_time.isoformat()
         tray_data["last_used"] = local_time.strftime("%d.%m.%Y %H:%M")
       else:
           tray_data["last_used"] = "-"
